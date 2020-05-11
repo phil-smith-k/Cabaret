e4ea1549f1f958b16cabc1531aa49308f91dfc69
Philip Smith <philip.smith.k@gmail.com>
2020-05-11 14:32:19 -0400

    countdown added

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const form = document.querySelector('.google-form iframe');
 const contributeBtn = document.querySelector('#contribute-btn');
 const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.navbar-nav');
-
+const countdown = document.querySelector('.countdown');
+const eventDate = new Date("Jul 31, 2020 19:00:00").getTime();
 
 
 function toggleFormDisplay() {
@@ -32,5 +33,17 @@ function toggleNavMenu(e) {
     }
 }
 
+function countdownHandler() {
+    let now = new Date().getTime();
+    let span = eventDate - now;
+    let days = Math.floor(span / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((span % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((span % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((span % (1000 * 60)) / 1000);
+    
+    countdown.textContent = `${days} Days ${hours} Hours ${minutes} Minutes ${seconds} Seconds`;
+}
+
 contributeBtn.addEventListener('click', toggleFormDisplay);
 menuIcon.addEventListener('click', toggleNavMenu);
+window.setInterval(countdownHandler, 1000);
